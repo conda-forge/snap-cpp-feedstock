494f99433368a5e907e21caf1b6367a82f98c141
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-12-31 04:47:45 -0500

    Incorporate flags from `Makefile.config`

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,9 @@
 if [[ "`uname`" == "Linux" ]]
 then
+    export CXXFLAGS="${CXXFLAGS} -fopenmp"
     export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib"
 fi
+
+export CXXFLAGS="${CXXFLAGS} -Wall -Wno-unknown-pragmas -O3 -DNDEBUG"
 
 make all CC="${CXX}" CXX="${CXX}" LD="${LD}" CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}"
